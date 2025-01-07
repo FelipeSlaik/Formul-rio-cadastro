cb01513c251053c43bdf01845325d6c83f061597
FelipeSlaik <felipeslaik@gmail.com>
2025-01-07 09:47:20 -0300

    Inicio da validação

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-const formulario = document.getElementById('form');
+const emailInput = document.getElementById('email');
+const erroEmail = document.getElementById('erro-email');
+const formSubmit = document.getElementById('formulario');
 
-formulario.addEventListener("submit", function (event){
-    let formValido = true;
+const nameInput = document.getElementById('email');
+const erroName = document.getElementById('erro-nome')
 
-    const nome = document.getElementById('nome');
-    const erroNome = document.getElementById('erroNome')
-    if(nome.value.trim () === ''){
-        erroNome.textContent = 'Insira seu nome completo.';
-        formValido = false;
-    } else {
-        erroNome.textContent = '';
+   formSubmit.addEventListener('submit', function(e){
+    e.preventDefault();
+    const emailValue = emailInput.value;
+    const emailValid = emailValidate(emailValue);
+    const nameValue = nameInput.value;
+    const nameValid = nameValidate(nameValue)
+
+    if(emailValid){
+        erroEmail.textContent =  "";
+    }else{
+        erroEmail.textContent = "E-mail inválido"
+    }
+
+    if(nameValid){
+        erroName.textContent = "";
+    }else {
+        erroName.textContent = "Nome inválido"
+    }
+
+    });
+
+    const emailValidate = (emailInput) =>{
+        const regex = /^[^\s]+@[^\s]+\.[^\s]+$/;
+        return regex.test(emailInput);
+    }
+
+    const nameValidate = (nameInput) =>{
+        return nameInput.trim().length > 2;
     }
 
 
-})
+
+    
+
